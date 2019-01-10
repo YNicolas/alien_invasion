cffcbef07e6f05e5d550c070def609a84dc91118
yujingzhong <yujingzhong@trustkernel.com>
2019-01-10 14:49:58 +0800

    add settings class

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,14 +2,16 @@
 import sys
 import pygame
 
+from settings import Settings
+
 def run_game():
     #Initialize game & creat a screen object
     pygame.init()
-    screen = pygame.display.set_mode((1000,600))
-    pygame.display.set_caption("Alien Invasion")
 
-    #set the background color
-    bg_color = (230, 230, 230)
+    ai_settings = Settings()
+    screen = pygame.display.set_mode((ai_settings.screen_width,
+        ai_settings.screen_height))
+    pygame.display.set_caption("Alien Invasion")
 
     #start game loop
     while True:
@@ -19,7 +21,8 @@ def run_game():
                 sys.exit()
 
         #Refresh screen
-        screen.fill(bg_color)
+        #set the background color
+        screen.fill(ai_settings.bg_color)
         pygame.display.flip()
 
 run_game()
